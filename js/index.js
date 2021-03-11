82720136970c867c868315df1ed02691744a6199
eunbinzz <77326519+eunbinzz@users.noreply.github.com>
2021-03-11 15:25:51 +0900

    ver.1.5

--- js/index.js
+++ js/index.js
@@ -16,11 +16,14 @@
 // }
 
 // 로딩 애니메이션2
-$(window).on('load',function(){
-  setTimeout(function(){$('.introBox').fadeOut(500)}, 2000)  
+$(window).on('load', function(){
+    setTimeout(function(){
+        $('.introBox').fadeOut(500)
+    }, 1000)
 })
-
-$('.introBox').on('click', function(){$(this).fadeOut(100)})
+$('.introBox').on('click', function(){
+    $(this).fadeOut(100)
+})
 
 
 
@@ -75,16 +78,21 @@ function draw(jumsu, cname){
     
 }
 
-$(window).on('scroll',function(){
+$(window).on('scroll', function(){
     var sct = $(this).scrollTop()
-    if (sct < portNear){
-        $('.depth1 > li').eq(0).addClass('on').siblings().removeClass('on')    
-    } else if (sct >= portNear && sct < skillNear){
-        $('.depth1 > li').eq(1).addClass('on').siblings().removeClass('on')  
-        $('.skillContainer').removeClass('on').find('.myscore').css({ height:'0%'})  
-    }else if (sct >= skillNear && sct < lastNear){
-        $('.depth1 > li').eq(2).addClass('on').siblings().removeClass('on')
-        if(!$('.skillContainer').hasClass('on')){
+    if ( sct < portNear ) {
+        $('.depth1 > li').eq(0).addClass('on')
+        .siblings().removeClass('on')
+    } else if ( sct>=portNear && sct<skillNear ) {
+        $('.depth1 > li').eq(1).addClass('on')
+        .siblings().removeClass('on')
+        $('.skillContainer').removeClass('on').find('.myscore').css({
+            height:'0%'
+        })
+    } else if ( sct>=skillNear && sct<lastNear ) {
+        $('.depth1 > li').eq(2).addClass('on')
+        .siblings().removeClass('on')
+        if ( !$('.skillContainer').hasClass('on') ) {
             $('.skillContainer').addClass('on')
             draw(90, '.html')
             draw(80, '.css')
@@ -93,20 +101,96 @@ $(window).on('scroll',function(){
             draw(50, '.react')
         }
     } else {
-        $('.depth1 > li').eq(3).addClass('on').siblings().removeClass('on') 
-        $('.skillContainer').removeClass('on').find('.myscore').css({ height:'0%'}) 
+        $('.depth1 > li').eq(3).addClass('on')
+        .siblings().removeClass('on')
+        $('.skillContainer').removeClass('on').find('.myscore').css({
+            height:'0%'
+        })
     }
+
 
 })
 
 
 $('.section').on('mousewheel', function(e, delta){
-    //0보다 크면 위로, 0보다 작으면 아래로
-    if (delta > 0) {
-       var prev = $(this).prev().offset().top
-       $('html').stop().animate({scrollTop:prev}, 500, 'linear')
-    } else if (delta < 0) {
+    // 0보다 크면 위로, 0보다 작으면 아래로
+    if (delta>0) {  
+        var prev = $(this).prev().offset().top
+        $('html').stop().animate({
+            scrollTop:prev
+        }, 500, 'linear')
+    } else if (delta<0) {
         var next = $(this).next().offset().top
-        $('html').stop().animate({scrollTop:next}, 500, 'linear')
+        $('html').stop().animate({
+            scrollTop:next
+        }, 500, 'linear')
     }
+})
+
+    //사진 클릭시 팝업 모달박스 나오도록
+var linum
+$('.gallery li').on('click', function(e){
+    e.preventDefault()
+    linum = $(this).index()
+    var href = $(this).find('a').attr('href')
+    var src = $(this).find('img').attr('src')
+    var alt = $(this).find('img').attr('alt')
+    $('body').append('<div class="outbox"><div class="inbox"></div></div>')
+    $('.outbox').css({
+        position:'fixed', top:0, left:0, right:0, bottom:0,
+        zIndex:'999999', background:'rgba(0,0,0,0.8)'
+    })
+    $('.inbox').css({
+        position:'absolute', top:'50%', left:'50%', transform:'translate(-50%, -50%)'
+    })
+    .append(`<a href="${href}" target="_blank"><img src="${src}" alt="${alt}"></a>`)
+    .append('<button class="close"><i class="fas fa-times-circle"></i></button>')
+    .append('<button class="arrow prev"><i class="fas fa-angle-left"></i></button><button class="arrow next"><i class="fas fa-angle-right"></i></button>')
+    $('.inbox .close').css({
+        position:'absolute', top:'-10px', right:'-10px',
+        background:'none', border:'none', fontSize:'40px', color:'#fff'
+    })
+    $('.inbox .prev').css({
+        position:'absolute', top:'50%', left:'50%', marginLeft:'-330px', marginTop:'-20px',
+        background:'none', border:'none', fontSize:'40px', color:'#fff'
+    })
+    $('.inbox .next').css({
+        position:'absolute', top:'50%', right:'50%', marginRight:'-330px', marginTop:'-20px',
+        background:'none', border:'none', fontSize:'40px', color:'#fff'
+    })
+})
+
+$('body').on('click', '.inbox .close, .outbox', function(){
+    $('.outbox').remove()
+})
+
+$('body').on('click', '.inbox', function(e){
+    e.stopPropagation()
+})
+
+
+function gallery(indexnum){
+    var href = $('.gallery li').eq(indexnum).find('a').attr('href')
+    var src = $('.gallery li').eq(indexnum).find('img').attr('src')
+    var alt = $('.gallery li').eq(indexnum).find('img').attr('alt')
+    $('.inbox').find('a').attr({href:href})
+    $('.inbox').find('img').attr({src:src, alt:alt})
+}
+
+//사진 반복 7은 li갯수
+$('body').on('click', '.inbox .next', function(){
+    linum++
+    if (linum>7) {    //if (linum === $('.gallery li').length)으로 해도됨
+        linum = 0
+    }
+    gallery(linum)
+})
+
+
+$('body').on('click', '.inbox .prev', function(){
+    linum--
+    if (linum<0) {
+        linum = 7
+    }
+    gallery(linum)
 })
